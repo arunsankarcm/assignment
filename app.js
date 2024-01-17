6f0b803a7ab00e4a8b243698e4a2b451a73c2c09
Arun Sankar <sankarchevoor@gmail.com>
2024-01-18 00:56:14 +0530

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 
 var indexRouter = require('./routes/index');
@@ -30,6 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: 'http://localhost:3000' // Allow only the React app to access
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
